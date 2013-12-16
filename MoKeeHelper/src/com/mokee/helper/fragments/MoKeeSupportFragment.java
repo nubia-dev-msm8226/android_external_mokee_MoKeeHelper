7e96eb3883e5db2773c6f850f4fc607aa78d4da0
martincz <martincz@mokeedev.com>
2013-12-16 20:29:15 +0800

    MoKeeHelper: remove review link and add donate link
    Change-Id: I1941edd165ef3b73b057ae879531ec8fb5ed2175

--- MoKeeHelper/src/com/mokee/helper/fragments/MoKeeSupportFragment.java
+++ MoKeeHelper/src/com/mokee/helper/fragments/MoKeeSupportFragment.java
@@ -33,20 +33,20 @@ public class MoKeeSupportFragment extends PreferenceFragment {
     private static final String KEY_MOKEE_ISSUES = "mokee_issues";
     private static final String KEY_MOKEE_STATISTICS = "mokee_statistics";
     private static final String KEY_MOKEE_CHANGELOG = "mokee_changelog";
-    private static final String KEY_MOKEE_REVIEW = "mokee_review";
     private static final String KEY_MOKEE_GITHUB = "mokee_github";
     private static final String KEY_MOKEE_CONTRIBUTOR = "mokee_contributor";
     private static final String KEY_MOKEE_WIKI = "mokee_wiki";
+    private static final String KEY_MOKEE_DONATE = "mokee_donate";
 
     private static final String URL_MOKEE_WEBSITE = "http://www.mfunz.com";
     private static final String URL_MOKEE_FORUM = "http://bbs.mfunz.com";
     private static final String URL_MOKEE_ISSUES = "http://issues.mfunz.com";
     private static final String URL_MOKEE_STATISTICS = "http://stats.mfunz.com";
     private static final String URL_MOKEE_CHANGELOG = "http://changelog.mfunz.com";
-    private static final String URL_MOKEE_REVIEW = "http://review.mfunz.com";
     private static final String URL_MOKEE_GITHUB = "https://github.com/MoKee";
     private static final String URL_MOKEE_CONTRIBUTOR = "http://www.mfunz.com/contributors/";
     private static final String URL_MOKEE_WIKI = "http://wiki.mfunz.com";
+    private static final String URL_MOKEE_DONATE = "http://www.mfunz.com/donate/";
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -68,14 +68,14 @@ public class MoKeeSupportFragment extends PreferenceFragment {
             goToURL(URL_MOKEE_STATISTICS);
         } else if (key.equals(KEY_MOKEE_CHANGELOG)) {
             goToURL(URL_MOKEE_CHANGELOG);
-        } else if (key.equals(KEY_MOKEE_REVIEW)) {
-            goToURL(URL_MOKEE_REVIEW);
         } else if (key.equals(KEY_MOKEE_GITHUB)) {
             goToURL(URL_MOKEE_GITHUB);
         } else if (key.equals(KEY_MOKEE_CONTRIBUTOR)) {
             goToURL(URL_MOKEE_CONTRIBUTOR);
         } else if (key.equals(KEY_MOKEE_WIKI)) {
             goToURL(URL_MOKEE_WIKI);
+        } else if (key.equals(KEY_MOKEE_DONATE)) {
+            goToURL(URL_MOKEE_DONATE);
         }
         return super.onPreferenceTreeClick(preferenceScreen, preference);
     }
