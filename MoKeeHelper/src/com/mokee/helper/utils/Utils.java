66b245a247090da05ef602d350d6616f1c562fc8
wszfer <wszfer@gmail.com>
2013-12-24 00:19:03 +0800

    Replace method
    Change-Id: I41e05f5389bbe519ba6afc85207e6d17981a4a72

--- MoKeeHelper/src/com/mokee/helper/utils/Utils.java
+++ MoKeeHelper/src/com/mokee/helper/utils/Utils.java
@@ -342,26 +342,6 @@ public class Utils {
         }
     }
 
-    /**
-     * 转换文件大小
-     * 
-     * @param size
-     * @return
-     */
-    public static String formetFileSize(long size) {
-        DecimalFormat df = new DecimalFormat("#0.00");
-        String fileSizeString = "";
-        if (size < 1024) {
-            fileSizeString = df.format((double) size) + "B";
-        } else if (size < 1048576) {
-            fileSizeString = df.format((double) size / 1024) + "K";
-        } else if (size < 1073741824) {
-            fileSizeString = df.format((double) size / 1048576) + "M";
-        } else {
-            fileSizeString = df.format((double) size / 1073741824) + "G";
-        }
-        return fileSizeString;
-    }
 
     /**
      * 判断网络是否可用
