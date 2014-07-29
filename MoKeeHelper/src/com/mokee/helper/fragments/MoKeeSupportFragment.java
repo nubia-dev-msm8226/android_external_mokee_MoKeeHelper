28a472cfcf291eb2626d77d276088122a7b88f55
martincz <martincz@mokeedev.com>
2014-07-29 21:34:46 +0800

    MoKeeHelper: remove wiki and statistics
    Change-Id: I7189191af46de55ea48d3750d8f93b329a2ff883

--- MoKeeHelper/src/com/mokee/helper/fragments/MoKeeSupportFragment.java
+++ MoKeeHelper/src/com/mokee/helper/fragments/MoKeeSupportFragment.java
@@ -38,21 +38,17 @@ public class MoKeeSupportFragment extends PreferenceFragment {
     private static final String KEY_MOKEE_WEBSITE = "mokee_website";
     private static final String KEY_MOKEE_FORUM = "mokee_forum";
     private static final String KEY_MOKEE_ISSUES = "mokee_issues";
-    private static final String KEY_MOKEE_STATISTICS = "mokee_statistics";
     private static final String KEY_MOKEE_CHANGELOG = "mokee_changelog";
     private static final String KEY_MOKEE_BUILD_STATUS = "mokee_build_status";
     private static final String KEY_MOKEE_GITHUB = "mokee_github";
-    private static final String KEY_MOKEE_WIKI = "mokee_wiki";
     private static final String KEY_MOKEE_DONATE = "mokee_donate";
 
     private static final String URL_MOKEE_WEBSITE = "http://www.mokeedev.com";
     private static final String URL_MOKEE_FORUM = "http://bbs.mfunz.com";
     private static final String URL_MOKEE_ISSUES = "http://issues.mokeedev.com";
-    private static final String URL_MOKEE_STATISTICS = "http://stats.mokeedev.com";
     private static final String URL_MOKEE_CHANGELOG = "http://changelog.mokeedev.com";
     private static final String URL_MOKEE_BUILD_STATUS = "http://build.mokeedev.com";
     private static final String URL_MOKEE_GITHUB = "https://github.com/MoKee";
-    private static final String URL_MOKEE_WIKI = "http://wiki.mfunz.com";
     public static final String URL_MOKEE_DONATE = "http://www.mokeedev.com/donate/";
 
     private Activity mContext;
@@ -74,16 +70,12 @@ public class MoKeeSupportFragment extends PreferenceFragment {
             goToURL(mContext, URL_MOKEE_FORUM);
         } else if (key.equals(KEY_MOKEE_ISSUES)) {
             goToURL(mContext, URL_MOKEE_ISSUES);
-        } else if (key.equals(KEY_MOKEE_STATISTICS)) {
-            goToURL(mContext, URL_MOKEE_STATISTICS);
         } else if (key.equals(KEY_MOKEE_CHANGELOG)) {
             goToURL(mContext, URL_MOKEE_CHANGELOG);
         } else if (key.equals(KEY_MOKEE_BUILD_STATUS)) {
             goToURL(mContext, URL_MOKEE_BUILD_STATUS);
         } else if (key.equals(KEY_MOKEE_GITHUB)) {
             goToURL(mContext, URL_MOKEE_GITHUB);
-        } else if (key.equals(KEY_MOKEE_WIKI)) {
-            goToURL(mContext, URL_MOKEE_WIKI);
         } else if (key.equals(KEY_MOKEE_DONATE)) {
             goToURL(mContext, URL_MOKEE_DONATE);
         }
