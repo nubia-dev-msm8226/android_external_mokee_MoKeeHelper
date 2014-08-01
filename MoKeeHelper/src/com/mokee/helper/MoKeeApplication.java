6434cbc1195bb0fa49474fbcc746fba30cc4adf2
martincz <martincz@mokeedev.com>
2014-08-02 00:27:17 +0800

    MoKeeHelper: improve set alias and tags
    Change-Id: Iac1c979db8174a3271e83c670963baf12099a525

--- MoKeeHelper/src/com/mokee/helper/MoKeeApplication.java
+++ MoKeeHelper/src/com/mokee/helper/MoKeeApplication.java
@@ -20,11 +20,17 @@ package com.mokee.helper;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.mokee.util.MoKeeUtils;
+
 import android.app.Activity;
 import android.app.Application;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import cn.jpush.android.api.JPushInterface;
+import cn.jpush.android.api.TagAliasCallback;
 
 import com.android.settings.mkstats.Utilities;
 import com.mokee.helper.activities.MoKeeCenter;
@@ -32,24 +38,120 @@ import com.mokee.helper.utils.Utils;
 
 public class MoKeeApplication extends Application implements
         Application.ActivityLifecycleCallbacks {
+
     private static Context context;
     private boolean mMainActivityActive;
+    private SharedPreferences prefs;
+
+    private static final String TAG = "MoKeeApplication";
+    private static final String MKPUSH_PREF = "mokee_push";
+    private static final String MKPUSH_ALIAS = "pref_alias";
+    private static final String MKPUSH_TAGS = "pref_tags";
+
+    private static final int MSG_SET_ALIAS = 1001;
+    private static final int MSG_SET_TAGS = 1002;
+
+    private final Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(android.os.Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what) {
+                case MSG_SET_ALIAS:
+                    Log.d(TAG, "Set alias in handler.");
+                    JPushInterface.setAliasAndTags(getApplicationContext(), (String) msg.obj, null,
+                            mAliasCallback);
+                    break;
+                case MSG_SET_TAGS:
+                    Log.d(TAG, "Set tags in handler.");
+                    JPushInterface.setAliasAndTags(getApplicationContext(), null,
+                            (Set<String>) msg.obj, mTagsCallback);
+                    break;
+                default:
+                    Log.i(TAG, "Unhandled msg - " + msg.what);
+            }
+        }
+    };
 
     @Override
     public void onCreate() {
         super.onCreate();
+
         mMainActivityActive = false;
         registerActivityLifecycleCallbacks(this);
         context = getApplicationContext();
 
         // MoKeePush Interface
+        prefs = context.getSharedPreferences(MKPUSH_PREF, 0);
         JPushInterface.setDebugMode(false);
         JPushInterface.init(this);
+        // Set Alias
+        String alias = Utilities.getUniqueID(this);
+        String prefAlias = prefs.getString(MKPUSH_ALIAS, null);
+        if (!alias.equals(prefAlias)) mHandler.sendMessage(mHandler.obtainMessage(MSG_SET_ALIAS, alias));
+        // Set Tags
         Set<String> tags = new HashSet<String>();
         tags.add(Utils.getDeviceType());
-        tags.add(Utilities.getMoKeeVersion());
-        JPushInterface.setAliasAndTags(this, Utilities.getUniqueID(this), tags);
+        tags.add(Utilities.getMoKeeVersion().replace(".", ""));
+        Set<String> prefTags = prefs.getStringSet(MKPUSH_TAGS, null);
+        if (!tags.equals(prefTags)) mHandler.sendMessage(mHandler.obtainMessage(MSG_SET_TAGS, tags));
     }
+
+    private final TagAliasCallback mAliasCallback = new TagAliasCallback() {
+
+        @Override
+        public void gotResult(int code, String alias, Set<String> tags) {
+            String logs;
+            switch (code) {
+                case 0:
+                    logs = "Set alias success";
+                    prefs.edit().putString(MKPUSH_ALIAS, alias).apply();
+                    Log.i(TAG, logs);
+                    break;
+                case 6002:
+                    logs = "Failed to set alias due to timeout. Try again after 60s.";
+                    Log.i(TAG, logs);
+                    if (MoKeeUtils.isOnline(getContext())) {
+                        mHandler.sendMessageDelayed(mHandler.obtainMessage(MSG_SET_ALIAS, alias),
+                                1000 * 60);
+                    } else {
+                        Log.i(TAG, "No network");
+                    }
+                    break;
+                default:
+                    logs = "Failed with errorCode = " + code;
+                    Log.e(TAG, logs);
+            }
+        }
+    };
+
+    private final TagAliasCallback mTagsCallback = new TagAliasCallback() {
+
+        @Override
+        public void gotResult(int code, String alias, Set<String> tags) {
+            String logs;
+            switch (code) {
+                case 0:
+                    logs = "Set tag success";
+                    prefs.edit().putStringSet(MKPUSH_TAGS, tags).apply();
+                    Log.i(TAG, logs);
+                    break;
+                case 6002:
+                    logs = "Failed to set tags due to timeout. Try again after 60s.";
+                    Log.i(TAG, logs);
+                    if (MoKeeUtils.isOnline(getContext())) {
+                        mHandler.sendMessageDelayed(mHandler.obtainMessage(MSG_SET_TAGS, tags),
+                                1000 * 60);
+                    } else {
+                        Log.i(TAG, "No network");
+                    }
+                    break;
+                default:
+                    logs = "Failed with errorCode = " + code;
+                    Log.e(TAG, logs);
+            }
+        }
+
+    };
 
     public static Context getContext() {
         return context;
