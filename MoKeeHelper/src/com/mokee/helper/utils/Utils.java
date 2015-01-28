b2430424726589754eb76611c223b3861232fd91
martincz <martincz@mokeedev.com>
2015-01-28 12:50:40 +0800

    Use more current logic to determine install path
    Change-Id: Id330c18cc426e65dd66bff35b0cef5a04040ce89

--- MoKeeHelper/src/com/mokee/helper/utils/Utils.java
+++ MoKeeHelper/src/com/mokee/helper/utils/Utils.java
@@ -137,11 +137,12 @@ public class Utils {
 
     public static void triggerUpdate(Context context, String updateFileName) throws IOException {
         /*
-         * Should perform the following steps. 1.- mkdir -p /cache/recovery 2.-
-         * echo 'boot-recovery' > /cache/recovery/command 3.- if(mBackup) echo
-         * '--nandroid' >> /cache/recovery/command 4.- echo
-         * '--update_package=SDCARD:update.zip' >> /cache/recovery/command 5.-
-         * reboot recovery
+         * Should perform the following steps.
+         * 1.- mkdir -p /cache/recovery
+         * 2.- echo 'boot-recovery' > /cache/recovery/command
+         * 3.- if(mBackup) echo '--nandroid'  >> /cache/recovery/command
+         * 4.- echo '--update_package=SDCARD:update.zip' >> /cache/recovery/command
+         * 5.- reboot recovery
          */
 
         // Set the 'boot recovery' command
@@ -151,22 +152,19 @@ public class Utils {
         os.write("echo 'boot-recovery' >/cache/recovery/command\n".getBytes());
 
         // See if backups are enabled and add the nandroid flag
-        /*
-         * TODO: add this back once we have a way of doing backups that is not
-         * recovery specific if (mPrefs.getBoolean(Constants.BACKUP_PREF, true))
-         * {
-         * os.write("echo '--nandroid'  >> /cache/recovery/command\n".getBytes(
-         * )); }
-         */
+        /* TODO: add this back once we have a way of doing backups that is not recovery specific
+           if (mPrefs.getBoolean(Constants.BACKUP_PREF, true)) {
+           os.write("echo '--nandroid'  >> /cache/recovery/command\n".getBytes());
+           }
+           */
 
         // Add the update folder/file name
         // Emulated external storage moved to user-specific paths in 4.2
-        String userPath = Environment.isExternalStorageEmulated() ? ("/" + UserHandle.myUserId())
-                : "";
+        String userPath = Environment.isExternalStorageEmulated() ? ("/" + UserHandle.myUserId()) : "";
 
-        String cmd = "echo '--update_package=" + getStorageMountpoint(context) + userPath + "/"
-                + Constants.UPDATES_FOLDER + "/" + updateFileName
-                + "' >> /cache/recovery/command\n";
+        String cmd = "echo '--update_package=" + getStorageMountpoint(context) + userPath
+            + "/" + Constants.UPDATES_FOLDER + "/" + updateFileName
+            + "' >> /cache/recovery/command\n";
         os.write(cmd.getBytes());
         os.flush();
 
@@ -179,22 +177,22 @@ public class Utils {
         StorageManager sm = (StorageManager) context.getSystemService(Context.STORAGE_SERVICE);
         StorageVolume[] volumes = sm.getVolumeList();
         String primaryStoragePath = Environment.getExternalStorageDirectory().getAbsolutePath();
-        boolean alternateIsInternal = context.getResources().getBoolean(R.bool.alternateIsInternal);
-
-        if (volumes.length <= 1) {
-            // single storage, assume only /sdcard exists
-            return "/sdcard";
-        }
 
         for (int i = 0; i < volumes.length; i++) {
             StorageVolume v = volumes[i];
             if (v.getPath().equals(primaryStoragePath)) {
-                if (!v.isRemovable() && alternateIsInternal) {
-                    return "/emmc";
+                /* This is the primary storage, where we stored the update file
+                 *
+                 * For MK50, a non-removable storage (partition or FUSE)
+                 * will always be primary. But we have devices out there in which
+                 * /sdcard is the microSD, and/or an emmc partition
+                 */
+                if (!v.isRemovable() && Environment.isExternalStorageEmulated()) {
+                    return "/data/media";
                 }
-            }
+            };
         }
-        // Not found, assume non-alternate
+        // Not found, assume non-datamedia device
         return "/sdcard";
     }
 
